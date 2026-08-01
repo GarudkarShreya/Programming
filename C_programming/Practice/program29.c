040a0707ec001b0f992112e24543ddd164d5a89c
GarudkarShreya <shreya.y.garudkar@gmail.com>
2026-08-02 00:47:20 +0530

    Add files via upload

---
+++ C_programming/Practice/program29.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    //Input updator
+    
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+
+    int iCnt = 0;
+
+    for(iCnt = 1;iCnt <= iNo;iCnt++)
+    {
+        printf("Jay ganesh\n");
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter the frequency : \n");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+    return 0;
+}
