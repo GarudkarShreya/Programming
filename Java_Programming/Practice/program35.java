b6868dee382612ac5d9864155dbeedebb39aa319
GarudkarShreya <shreya.y.garudkar@gmail.com>
2026-08-02 04:52:49 +0530

    Add files via upload

---
+++ Java_Programming/Practice/program35.java
@@ -0,0 +1,8 @@
+class program35`
+
+{
+    public static void main(String[] args)
+    {
+       System.out.println("Jay Ganesh ");
+    } 
+}
---
+++ Java_Programming/Practice/program40.java
@@ -0,0 +1,33 @@
+//TYPE 2
+
+import java.util.Scanner;
+class program40
+
+{
+    void CheckDivisible(int iNo)
+    {
+        if((iNo % 3 == 0) && (iNo % 5 == 0))
+        {
+            System.out.println("Number is divisible by 3 and 5");
+        }
+        else
+        {
+            System.out.println("Number is not divisible by 3 and 5");
+        }
+
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iValue = 0;
+
+        System.out.println("Enter number : ");
+        sobj.nextInt();
+
+        CheckDivisible(iValue);      //error
+
+
+    }
+}
