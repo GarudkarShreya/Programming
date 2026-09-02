dfe6a794fd58be0c50be4374fe20f8a2002b73e2
GarudkarShreya <shreya.y.garudkar@gmail.com>
2026-09-02 19:56:35 +0530

    Add files via upload

---
+++ C_programming/Practice/program257.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int CountSpace(const char *str)
+{
+    int iCount = 0;
+    int Arr[0];
+
+   while (*str != 0)
+   {
+    
+    if (*str == ' ')
+    {
+      iCount++;
+    }
+     str++;
+   }
+   return iCount;
+}
+
+int main()
+{
+    char Arr[50] = {'\0'};
+    int iRet = 0;
+
+    printf("Enter string : \n");
+    scanf("%[^'\n']s",Arr); 
+    
+    iRet = CountSpace(Arr);
+
+    printf("Frequencey is : %d\n",iRet);
+
+    return 0;
+}
