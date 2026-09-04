f35d39f0096e9a58f8e424108f1ad7c4b18810ab
GarudkarShreya <shreya.y.garudkar@gmail.com>
2026-09-05 01:14:23 +0530

    Add files via upload

---
+++ Java_Programming/Practice/program276.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+
+class program276
+{
+    public static void main(String A[])
+    {
+        
+        Scanner sobj = new Scanner(System.in);
+        String data = null;
+        int iRet = 0;
+
+        StringX strobj = new StringX();   // ERROR
+
+        System.out.println("Enter String : ");
+        data = sobj.nextLine();
+
+        iRet = strobj.CountCapital(data);
+        System.out.println("Number of capital character : "+iRet);
+
+        
+        iRet = strobj.CountSmall(data);
+        System.out.println("Number of small character : "+iRet);
+
+        iRet = strobj.CountDigits(data);
+        System.out.println("Number of Digits are : "+iRet);
+        
+        iRet = strobj.CountSpace(data);
+        System.out.println("Number of Spaces are : "+iRet);
+
+        iRet = strobj.CountSpecial(data);
+        System.out.println("Number of Special symbols are : "+iRet);
+
+
+    }
+}
